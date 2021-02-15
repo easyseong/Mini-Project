7627bfaab3f187a3cf5d9859051e7a95c98ec481
leejiseong <leejiseong@DESKTOP-AKI7KFF>
2021-02-16 02:27:31 +0900

    단어 맞추기 게임

--- src/Mini_Project_250.java
+++ src/Mini_Project_250.java
@@ -4,26 +4,31 @@ import java.util.Scanner;
 public class Mini_Project_250 {
 
 	public static void main(String[] args) {
+
 		Scanner s = new Scanner(System.in);
 		String[] words = {"java", "count","school","book","student","programmer"};
-		
+		int num=0;
 		int index = (new Random()).nextInt(words.length);
 		String solution = words[index];
 		StringBuffer answer = new StringBuffer(solution.length());//정답짝대기갯수
-		
-		
-		System.out.print("현재의 상태: ");
-		for(int i=0;i<solution.length();i++) 
-			System.out.print("_ ");
-		System.out.println();
-		System.out.print("글자를 추측하시오: ");
-		char x = s.next().charAt(0);	//next()로 입력받고 char형 변수를 꺼내온다.
-		
-		for(int i=0;i<solution.length();i++) {
-			if(solution.charAt(i)==x) {
-				
+		for(int i=0;i<solution.length();i++)
+			answer.append('-');
+		do {
+			System.out.println("현재의 상태: "+answer);
+			System.out.print("글자를 추측하시오: ");
+			char x = s.next().charAt(0);	//next()로 입력받고 char형 변수를 꺼내온다.
+	
+			for(int i=0;i<solution.length();i++) {
+				if(solution.charAt(i)==x) {
+					answer.delete(i, i+1);
+					answer.insert(i, x);
+					num++;
+				}	
 			}
-		}
+		}while(num!=solution.length());
+		System.out.println(answer);
+		System.out.println("성공");
+
 
 	}
 
